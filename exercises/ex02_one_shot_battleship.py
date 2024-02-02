f51e0d8a2d972098bcd741eb6c751fd99a9db07c
Sherry Ramos Martinez <saramos@unc.edu>
2024-02-01 19:01:39 -0500

    try 2 ex02 one shot battleship

--- exercises/ex02_one_shot_battleship.py
+++ exercises/ex02_one_shot_battleship.py
@@ -1,5 +1,5 @@
 """One shot battleship!"""
-__author__ = 730711248
+__author__ = "730711248"
 
 GRID: int = 4
 SECRET_ROW: int = 3
@@ -8,25 +8,27 @@ SECRET_COLUMN: int = 2
 invalid_row: bool = False
 invalid_column: bool = False
 
-guess_row: int = int(input(f"Guess a row: "))
-if not(1 <= guess_row <= GRID):
-    invalid_row: bool = True
+# user input row
+guess_row: int = int(input("Guess a row: "))
+if not (1 <= guess_row <= GRID):
+    invalid_row = True
 
-#if user row guess is too high/low allow user to guess again until valid
+# if user row guess is too high/low allow user to guess again until valid
 while invalid_row:
-    if not(1 <= guess_row <= GRID):
+    if not (1 <= guess_row <= GRID):
         new_row: int = int(input(f"The grid is only {GRID} by {GRID}. Try again: "))
     if 1 <= new_row <= GRID: 
         invalid_row = False
         guess_row = new_row
 
-guess_column: int = int(input(f"Guess a column: "))
-if not(1 <= guess_column <= GRID):
-    invalid_column: bool = True
+# user input column
+guess_column: int = int(input("Guess a column: "))
+if not (1 <= guess_column <= GRID):
+    invalid_column = True
 
-#if user column guess is too high/low allow user to guess again until valid
+# if user column guess is too high/low allow user to guess again until valid
 while invalid_column:
-    if not(1 <= guess_column < GRID):
+    if not (1 <= guess_column < GRID):
         new_column: int = int(input(f"The grid is only {GRID} by {GRID}. Try again: "))
     if 1 <= new_column <= GRID: 
         invalid_column = False
@@ -36,38 +38,38 @@ BLUE_BOX: str = "\U0001F7E6"
 RED_BOX: str = "\U0001F7E5"
 WHITE_BOX: str = "\U00002B1C"
 
-#if guess is hit (red box) or miss (white box)
+# if guess is hit (red box) or miss (white box)
 result: str = WHITE_BOX
 if guess_row == SECRET_ROW and guess_column == SECRET_COLUMN:
     result = RED_BOX
 
-#printing the boxes
-row_indx: int = 1
-while row_indx <= GRID: #row while loop
-    emojis: str =""
-    column_indx: int = 1
-    if guess_row == row_indx: 
-        while column_indx <= GRID: #column while loop
-            if guess_column == column_indx:
-                emojis = emojis + result
+# printing the boxes
+row_idx: int = 1
+while row_idx <= GRID:  # row while loop
+    emojis: str = ""
+    column_idx: int = 1
+    if guess_row == row_idx: 
+        while column_idx <= GRID:  # column while loop
+            if guess_column == column_idx:
+                emojis += result
             else:
-                emojis = emojis + BLUE_BOX
-            column_indx += 1
+                emojis += BLUE_BOX
+            column_idx += 1
     else: 
-        while column_indx <= GRID:
-            emojis = emojis + BLUE_BOX
-            column_indx += 1
+        while column_idx <= GRID:
+            emojis += BLUE_BOX
+            column_idx += 1
     print(emojis)
-    row_indx += 1
+    row_idx += 1
 
-#if user guess is correct
+# if user guess is correct
 if guess_row == SECRET_ROW and guess_column == SECRET_COLUMN:
-    print(f"Hit!")
-    #user row guess is correct
+    print("Hit!")
+    # user row guess is correct
 elif guess_row == SECRET_ROW and guess_column != SECRET_COLUMN:
-    print(f"Close! Correct row, wrong column.")
-    #user column guess is correct
+    print("Close! Correct row, wrong column.")
+    # user column guess is correct
 elif guess_row != SECRET_ROW and guess_column == SECRET_COLUMN:
-    print(f"Close! Correct column, wrong row.")
+    print("Close! Correct column, wrong row.")
 else:
-    print(f"Miss!")
+    print("Miss!")
